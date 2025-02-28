82d2e5a32676cd734f8eafc00d4e2accb94f0c60
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-02-28 13:07:35 +0100

    Updates Multimedia.java

--- src/entities/Multimedia.java
+++ src/entities/Multimedia.java
@@ -9,12 +9,4 @@ public abstract class Multimedia {
         this.title = title;
     }
 
-    //    metodi
-    private void play() {
-        System.out.println(this.title);
-    }
-
-    private void show() {
-        System.out.println(this.title);
-    }
 }
